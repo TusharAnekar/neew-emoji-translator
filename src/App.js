b8af85cc9811ac674642cf7704ba8cc9035b0c55
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-09-10 15:32:09 +0530

    added image, emojis and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "🪔": "Diya Lamp",
+  "⚽": "Football",
+  "🏈": "American Football",
+  "🏉": "Rugby Football",
+  "🐵": "Monkey Face",
+  "🐶": "Dog Face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
 
 function App() {
+
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We don't have transaltion of this emoji in our dictionary.";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Header'>
+        <h1> Emoji Translator</h1>
+        <p>This application helps user to find out what is the meaning of 12 emojis.</p>
+      </div>
+
+      <img src="/images/emoji.jpg"></img>
+
+      <input onChange={emojiInputHandler}></input>
+      <h2>{meaning}</h2>
+      <h3>Emojis we know:</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "1.5rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+
+      <footer>
+      <h3>Connect with me here:</h3>
+        <a class="footer-links" href="https://www.linkedin.com/in/tushar-anekar-435033a9/" target="_blank" rel="noopener norefferrer">LinkedIn</a>
+        <a class="footer-links" href="https://twitter.com/TusharAnekar" target="_blank" rel="noopener norefferrer">Twitter</a>
+        <a class="footer-links" href="https://github.com/TusharAnekar" target="_blank" rel="noopener norefferrer">GitHub</a>
+      </footer>
     </div>
   );
 }
